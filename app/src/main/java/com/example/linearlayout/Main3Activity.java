f6dba6601a2f59036e369dac97fdaae955a8af3e
ASUS <dewicahyanti751@gmail.com>
2020-05-12 01:32:58 +0800

    LatihanLayout

---
+++ app/src/main/java/com/example/linearlayout/Main3Activity.java
@@ -0,0 +1,25 @@
+package com.example.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+ Button Pindah;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        Pindah=findViewById(R.id.btnPindah2);
+
+        Pindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main3Activity.this, Main4Activity.class));
+            }
+        });
+    }
+}
